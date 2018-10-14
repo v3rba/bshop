43cf04042887f7515741ac94df5db3c5a8e6e907
v3rba <verbush@gmail.com>
2018-10-14 15:14:24 +0300

    Final edits

--- Gemfile
+++ Gemfile
@@ -7,5 +7,5 @@ gem "sinatra-activerecord"
 gem "sinatra-contrib"
 
 group :development do
-  gem "tux"
+	gem "tux"
 end
--- app.rb
+++ app.rb
@@ -6,8 +6,51 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
+	validates :name, presence: true, length: { minimum: 3 }
+	validates :phone, presence: true
+	validates :datestamp, presence: true
+	validates :color, presence: true
+end
+
+class Barber < ActiveRecord::Base
+end
+
+before do
+	@barbers = Barber.all
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index			
 end
+
+get '/visit' do
+	@c = Client.new
+	erb :visit
+end
+
+post '/visit' do
+
+	@c = Client.new params[:client]
+	if @c.save
+		erb "<h2>Спасибо, вы записались!</h2>"
+	else
+		@error = @c.errors.full_messages.first
+		erb :visit
+	end
+end
+
+get '/barber/:id' do
+	@barber = Barber.find(params[:id])
+	erb :barber
+end
+
+get '/bookings' do
+	@clients = Client.order('created_at DESC')
+	erb :bookings
+end
+
+get '/client/:id' do
+	@client = Client.find(params[:id])
+	erb :client
+end
+
